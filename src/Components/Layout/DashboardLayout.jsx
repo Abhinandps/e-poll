c955db59258f5bd6dad8a1cec1e17c9306f33b82
Abhinand <psabhinand333s@gmail.com>
2022-12-14 12:31:21 +0530

    Layout updated

--- src/Components/Layout/DashboardLayout.jsx
+++ src/Components/Layout/DashboardLayout.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { NavLink, Link, Outlet } from 'react-router-dom'
-import NotificationIcon from "../../../Assets/Icons/notification.svg"
-import bubbleIcon from "../../../Assets/Icons/bubble.svg"
-import avatar from "../../../Assets/Images/avatar.png"
-import icon from "../../../Assets/Icons/Icon.svg"
-import subIcon from "../../../Assets/Icons/sub-icon.svg"
-import arrow from "../../../Assets/Icons/toggleArrow.svg"
-import menuBar from "../../../Assets/Icons/menuBar.svg"
+import NotificationIcon from "../../Assets/Icons/notification.svg"
+import bubbleIcon from "../../Assets/Icons/bubble.svg"
+import avatar from "../../Assets/Images/avatar.png"
+import icon from "../../Assets/Icons/Icon.svg"
+import subIcon from "../../Assets/Icons/sub-icon.svg"
+import arrow from "../../Assets/Icons/toggleArrow.svg"
+import menuBar from "../../Assets/Icons/menuBar.svg"
 
-import "../../../Styles/DashboardLayout.css"
+import "../../Styles/DashboardLayout.css"
 
 
 const DashboardLayout = () => {
@@ -18,6 +18,8 @@ const DashboardLayout = () => {
     return (
         <>
             <div className='dashboard-container'>
+
+                {/* Top Bar */}
                 <div className="dashboard-topbar-section">
                     <div className="topbar-title-section">
                         <img src={menuBar} alt="" />
@@ -37,23 +39,28 @@ const DashboardLayout = () => {
                             <h3>Mia V</h3>
                             <p>Student</p>
                         </div>
-                        <div className="topbar-userOptionField">
 
+                        <div className="topbar-userOptionField">
                             <img src={bubbleIcon} alt="" />
                         </div>
                     </div>
                 </div>
+
+                {/* Side Bar  */}
                 <div className="dashboard-sidebar-section">
                     <div className='toggle-button'>
                         <img src={arrow} alt="" />
                     </div>
                     <div className="dashboard-sidebar-logo">LOGO</div>
+
                     <div className="dashboard-sidebar-navigation-menu">
                         <NavLink index className="sidebar-navigation-list parentList">
                             <img src={icon} alt="" />
                             <p>Dashboard</p>
                         </NavLink >
+
                         <div className="sub-navigation-menu">
+                            
                             <Link className="sidebar-navigation-list childList">
                                 <img src={subIcon} alt="" />
                                 <p>Vice-Chairman</p>
@@ -71,10 +78,10 @@ const DashboardLayout = () => {
                     </div>
                 </div>
 
+                {/* Main  */}
                 <div className="innerContent-section">
                     <Outlet />
                 </div>
-                {/* */}
             </div>
         </>
     )
