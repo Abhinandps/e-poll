9cc60971f731e7b660219e9b1f74dec3ee15e03d
Abhinand <psabhinand333s@gmail.com>
2022-12-29 16:23:31 +0530

    view candidates

--- src/Routes/Admin/ViewCandidates.jsx
+++ src/Routes/Admin/ViewCandidates.jsx
@@ -1,9 +1,101 @@
 import React from 'react'
+import PositionCard from '../../Components/Cards/PositionCard'
 
 const ViewCandidates = () => {
+
+  const data = [
+    {
+      position: 'Chairman',
+      candidates: [
+        {
+          name: "Joshua Dyer",
+          department: "cs",
+          semester: "6",
+          rollno: "10"
+        },
+        {
+          name: "Jhon",
+          department: "cs",
+          semester: "6",
+          rollno: "11"
+        }
+
+      ]
+    },
+
+    {
+      position: 'Vice-Chairman',
+      candidates: [
+        {
+          name: "Joshua Dyer",
+          department: "cs",
+          semester: "6",
+          rollno: "12"
+        },
+        {
+          name: "Jhon",
+          department: "cs",
+          semester: "6",
+          rollno: "13"
+        },
+        {
+          name: "Joshua Dyer",
+          department: "cs",
+          semester: "6",
+          rollno: "14"
+        },
+        {
+          name: "Jhon",
+          department: "cs",
+          semester: "6",
+          rollno: "15"
+        }
+
+      ]
+    },
+    {
+      position: 'General Secretary',
+      candidates: [
+        {
+          name: "Joshua Dyer",
+          department: "cs",
+          semester: "6",
+          rollno: "16"
+        },
+        {
+          name: "Jhon",
+          department: "cs",
+          semester: "6",
+          rollno: "17"
+        },
+        {
+          name: "Joshua Dyer",
+          department: "cs",
+          semester: "6",
+          rollno: "18"
+        },
+        {
+          name: "Jhon",
+          department: "cs",
+          semester: "6",
+          rollno: "19"
+        }
+
+      ]
+    }
+
+  ]
+
+
   return (
-    <div>
-      View candidates
+    <div className='dashboard-main'>
+      <div className="election-view-section">
+        {data.map(position =>
+          <PositionCard {...position} key={position.position} />
+        )
+        }
+      </div>
+
     </div>
   )
 }
